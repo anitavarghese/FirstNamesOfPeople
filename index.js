bd3909c4bae223f627c8a6a5ee0caa545e00f740
anita <anita.joshy@gmail.com>
2022-10-10 12:30:48 +0000

    first names

---
+++ index.js
@@ -0,0 +1,8 @@
+const getFirstNames = require("../country/state/city/index");
+
+const listOfNames = require("../utilities/utils/index");
+
+function getPeopleInCity(listOfNames) {
+  return getFirstNames;
+}
+console.log(getPeopleInCity(listOfNames));
